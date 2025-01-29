f812a8230eaa8a4b681c750a7b452785f8ba0327
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2025-01-29 11:57:26 +0100

    update latestComment

--- front/src/components/Sections/LatestComments.jsx
+++ front/src/components/Sections/LatestComments.jsx
@@ -10,11 +10,19 @@ const LatestComments = () => {
 
   useEffect(() => {
     const fetchComments = async () => {
-      const response = await fetch(
-        `${process.env.REACT_APP_API_BASE_URL}/comment/latest`
-      );
-      const data = await response.json();
-      setComments(data);
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_BASE_URL}/comment/latest`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setComments(data);
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+        setComments([]);
+      }
     };
 
     fetchComments();
@@ -62,65 +70,66 @@ const LatestComments = () => {
       </Typography>
 
       <Slider {...settings}>
-        {comments.length > 0 ? (
-          comments.map((review) => (
-            <Box
-              key={review.id}
-              sx={{ display: "flex", justifyContent: "center" }}
-            >
-              <Card
-                sx={{
-                  width: { xs: 190, sm: 280, md: 310 },
-                  boxShadow: 3,
-                  borderRadius: 2,
-                  mx: 2,
-                  mb: 2,
-                  bgcolor: "#FFFFFF",
-                  cursor: "pointer",
-                  transition: "transform 0.2s, box-shadow 0.2s",
-                  "&:hover": {
-                    transform: "scale(1.05)",
-                    boxShadow: 6,
-                  },
-                }}
+        {comments.length > 0
+          ? comments.map((review) => (
+              <Box
+                key={review.id}
+                sx={{ display: "flex", justifyContent: "center" }}
               >
-                <CardContent>
-                  <Box sx={{ display: "flex", alignItems: "center", mb: 1 }}>
-                    {Array.from({ length: 5 }, (_, index) => (
-                      <StarIcon
-                        key={index}
-                        sx={{
-                          color:
-                            index < review.notation ? "#FFC700" : "#E0E0E0",
-                        }}
-                      />
-                    ))}
-                    <Typography variant="body2" sx={{ ml: 1, color: "#777" }}>
-                      {review.notation} / 5
+                <Card
+                  sx={{
+                    width: { xs: 190, sm: 280, md: 310 },
+                    boxShadow: 3,
+                    borderRadius: 2,
+                    mx: 2,
+                    mb: 2,
+                    bgcolor: "#FFFFFF",
+                    cursor: "pointer",
+                    transition: "transform 0.2s, box-shadow 0.2s",
+                    "&:hover": {
+                      transform: "scale(1.05)",
+                      boxShadow: 6,
+                    },
+                  }}
+                >
+                  <CardContent>
+                    <Box sx={{ display: "flex", alignItems: "center", mb: 1 }}>
+                      {Array.from({ length: 5 }, (_, index) => (
+                        <StarIcon
+                          key={index}
+                          sx={{
+                            color:
+                              index < review.notation ? "#FFC700" : "#E0E0E0",
+                          }}
+                        />
+                      ))}
+                      <Typography variant="body2" sx={{ ml: 1, color: "#777" }}>
+                        {review.notation} / 5
+                      </Typography>
+                    </Box>
+                    <Typography
+                      variant="body1"
+                      sx={{ fontWeight: "bold", mb: 0.5 }}
+                    >
+                      {review.user.pseudo}
                     </Typography>
-                  </Box>
-                  <Typography
-                    variant="body1"
-                    sx={{ fontWeight: "bold", mb: 0.5 }}
-                  >
-                    {review.user.pseudo}
-                  </Typography>
-                  <Typography variant="body2" sx={{ mb: 1, color: "#555" }}>
-                    {review.comment}
-                  </Typography>
-                  <Typography variant="caption" sx={{ color: "#999" }}>
-                    Créé le: {new Date(review.createdAt).toLocaleDateString()}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Box>
-          ))
-        ) : (
-          <Typography variant="h6" sx={{ textAlign: "center" }}>
-            Aucun avis disponible pour le moment.
-          </Typography>
-        )}
+                    <Typography variant="body2" sx={{ mb: 1, color: "#555" }}>
+                      {review.comment}
+                    </Typography>
+                    <Typography variant="caption" sx={{ color: "#999" }}>
+                      Créé le: {new Date(review.createdAt).toLocaleDateString()}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Box>
+            ))
+          : null}
       </Slider>
+      {comments.length === 0 && (
+        <Typography variant="h6" sx={{ textAlign: "center" }}>
+          Aucun avis disponible pour le moment.
+        </Typography>
+      )}
     </Box>
   );
 };
